0ab28ed72f85d573d906d1aa3be187723cd1d1a2
paula <paulabib17@gmail.com>
2021-10-25 19:26:03 +0200

    indetation is really importante no?

--- src/main/java/sncf/oui/io/core/main.java
+++ src/main/java/sncf/oui/io/core/main.java
@@ -6,11 +6,11 @@ import java.util.HashMap;
 
 public class main {
     public static void main(String[] args) {
-        HashMap<Integer,String> map = new HashMap<>();
-        map.put(3,"Fizz");
-        map.put(5,"Buzz");
-        map.put(7,"Bazz");
-        map.put(9,"Bouze");
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(3, "Fizz");
+        map.put(5, "Buzz");
+        map.put(7, "Bazz");
+        map.put(9, "Bouze");
         FizzBuzz fizzBuzz = new FizzBuzz(map);
 
         fizzBuzz.printTo(1000);
--- src/main/java/sncf/oui/io/fizzbuzz/FizzBuzz.java
+++ src/main/java/sncf/oui/io/fizzbuzz/FizzBuzz.java
@@ -1,35 +1,36 @@
 package sncf.oui.io.fizzbuzz;
 
-import java.util.*;
+import java.util.Iterator;
+import java.util.Map;
 
 public class FizzBuzz {
 
-    Map<Integer,String> map;
+    Map<Integer, String> map;
 
-    public FizzBuzz(Map<Integer,String> map){
+    public FizzBuzz(Map<Integer, String> map) {
         this.map = map;
     }
 
     public String valueOf(final int value) {
-        Iterator<Map.Entry<Integer,String>> it = map.entrySet().iterator();
+        Iterator<Map.Entry<Integer, String>> it = map.entrySet().iterator();
         boolean f = false;
         StringBuilder result = new StringBuilder();
         if (value < 0)
             throw new IllegalArgumentException("Negative value are not supported");
-        while(it.hasNext()) {
-            Map.Entry<Integer,String> pair = it.next();
+        while (it.hasNext()) {
+            Map.Entry<Integer, String> pair = it.next();
             if ((value % pair.getKey()) == 0) {
                 result.append(pair.getValue());
                 f = true;
             }
         }
-        if(f)
+        if (f)
             return result.toString();
         return Integer.toString(value);
     }
 
-    public void printTo(int value){
-        for(int i = 1 ; i <= value ; i++){
+    public void printTo(int value) {
+        for (int i = 1; i <= value; i++) {
             System.out.println(valueOf(i));
         }
     }
